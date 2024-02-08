8d5819644d4d702fdb8a47b11cc88436d892338a
Gabriel Jones <gabrieljones@users.noreply.github.com>
2024-02-07 22:12:11 -0500

    2023.06.2: elapsed time

--- 2023/src/06/`2`.scala
+++ 2023/src/06/`2`.scala
@@ -3,6 +3,7 @@ package `06`
 import java.io.File
 import java.lang.Math.log10
 import java.nio.file.{Path, Paths}
+import java.util.concurrent.TimeUnit
 import scala.io.Source
 import scala.util.chaining.scalaUtilChainingOps
 
@@ -21,13 +22,16 @@ object `2` extends App {
   for {
     lines: Iterator[String] <- entries
   } {
+    val start = System.nanoTime()
     lines.map(_.split(" +"))
       .map(_.drop(1))
       .map(_.tap(_.mkString(",").tap(println)))
       .map(_.mkString)
       .pipe { iter => iter.next().toInt -> iter.next().toLong }
       .tap(println)
-      .pipe{ case (t, d) => ( 0 to t ).map(_.toLong).map( x => x * (t - x) ).count( _ > d ) }
+      .pipe { case (t, d) => ( 0 to t ).map(_.toLong).toArray.map( x => x * (t - x) ).count( _ > d ) }
       .tap(println)
-    }
+    val end = System.nanoTime()
+    println(s"Elapsed time: ${(end - start).toDouble / TimeUnit.NANOSECONDS.convert(1, TimeUnit.SECONDS)}s")
   }
+}
