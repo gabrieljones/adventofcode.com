9c4fd06ffb4b262fd5925ef644b30b30bd80b9fb
Gabriel Jones <gabrieljones@users.noreply.github.com>
2024-12-02 01:31:23 -0500

    2024.01

---
+++ 2024/src/01/`1`.scala
@@ -0,0 +1,24 @@
+package `01`
+
+import java.io.File
+import java.nio.file.{Path, Paths}
+import scala.io.Source
+
+object `1` extends App {
+  val workingDir = Paths.get(".").resolve(Path.of(s"src/${this.getClass.getPackageName}"))
+  val inputs     = Seq(
+    "example",
+    "input",
+  )
+    .map(Path.of(_)).map(workingDir.resolve).map(_.toFile)
+
+  def srcs = inputs.map(Source.fromFile)
+
+  for (src <- srcs) {
+    val Seq(a, b) =  src.getLines().map(_.split("   ").map(_.toLong)).toSeq.transpose
+    val pairs = a.sorted.zip(b.sorted)
+    val diffs = pairs.map{ case (ae, be) => be - ae}.map(Math.abs)
+    val sum = diffs.sum
+    println(sum)
+  }
+}
---
+++ 2024/src/01/`2`.scala
@@ -0,0 +1,23 @@
+package `01`
+
+import java.io.File
+import java.nio.file.{Path, Paths}
+import scala.io.Source
+
+object `2` extends App {
+  val workingDir = Paths.get(".").resolve(Path.of(s"src/${this.getClass.getPackageName}"))
+  val inputs     = Seq(
+    "example",
+    "input",
+  )
+    .map(Path.of(_)).map(workingDir.resolve).map(_.toFile)
+
+  def srcs = inputs.map(Source.fromFile)
+
+  for (src <- srcs) {
+    val Seq(a, b) =  src.getLines().map(_.split("   ").map(_.toLong)).toSeq.transpose
+    val similarities = a.map(e => e * b.count(_ == e))
+    val similarity = similarities.sum
+    println(similarity)
+  }
+}
